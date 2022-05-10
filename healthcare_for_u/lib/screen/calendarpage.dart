b7d72c4e5f7e3c5ec2a6cf5a3ab59df08ccf9db1
Corinna Semenzato <corinna.semenzato@studenti.unipd.it>
2022-05-10 16:02:02 +0200

    Create calendar

--- healthcare_for_u/lib/screen/calendarpage.dart
+++ healthcare_for_u/lib/screen/calendarpage.dart
@@ -1,22 +1,113 @@
+import 'dart:collection';
+
 import 'package:flutter/material.dart';
 import 'package:healthcare_for_u/screen/profilepage.dart';
+import 'package:intl/intl.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-
+import 'package:table_calendar/table_calendar.dart';
+import '../models/event.dart';
 import 'homepage.dart';
 import 'healthpage.dart';
 
 // Card per ogni attività. Le attività si aggiorneranno a seconda dei dati
 // Ogni Card porta alla pagina di dettaglio dell'attività scelta.
 
-class CalendarPage extends StatelessWidget {
+/*class CalendarPage extends StatefulWidget {
   CalendarPage({Key? key}) : super(key: key);
 
   static const route = '/calendar';
   static const routename = 'Calendar Page';
 
   @override
+  State<CalendarPage> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  late final PageController _pageController;
+  late final ValueNotifier<List<Event>> _selectedEvents;
+  final ValueNotifier<DateTime> _focusedDay = ValueNotifier(DateTime.now());
+  final Set<DateTime> _selectedDays = LinkedHashSet<DateTime>(
+    equals: isSameDay,
+    hashCode: getHashCode,
+  );
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  RangeSelectionMode _rangeSelectionMode = RangeSelectionMode.toggledOff;
+  DateTime? _rangeStart;
+  DateTime? _rangeEnd;
+  DateTime? _selectedDay;
+
+  List<Event> _getEventsForDay(DateTime day) {
+    return kEvents[day] ?? [];
+  }
+
+  List<Event> _getEventsForDays(Iterable<DateTime> days) {
+    return [
+      for (final d in days) ..._getEventsForDay(d),
+    ];
+  }
+
+  List<Event> _getEventsForRange(DateTime start, DateTime end) {
+    final days = daysInRange(start, end);
+    return _getEventsForDays(days);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDays.add(_focusedDay.value);
+    _selectedEvents = ValueNotifier(_getEventsForDay(_focusedDay.value));
+  }
+
+  @override
+  void dispose() {
+    _focusedDay.dispose();
+    _selectedEvents.dispose();
+    super.dispose();
+  }
+
+  bool get canClearSelection =>
+      _selectedDays.isNotEmpty || _rangeStart != null || _rangeEnd != null;
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    setState(() {
+      if (_selectedDays.contains(selectedDay)) {
+        _selectedDays.remove(selectedDay);
+      } else {
+        _selectedDays.add(selectedDay);
+      }
+
+      _focusedDay.value = focusedDay;
+      _rangeStart = null;
+      _rangeEnd = null;
+      _rangeSelectionMode = RangeSelectionMode.toggledOff;
+    });
+
+    _selectedEvents.value = _getEventsForDays(_selectedDays);
+  }
+
+  void _onRangeSelected(DateTime? start, DateTime? end, DateTime focusedDay) {
+    setState(() {
+      _focusedDay.value = focusedDay;
+      _rangeStart = start;
+      _rangeEnd = end;
+      _selectedDays.clear();
+      _rangeSelectionMode = RangeSelectionMode.toggledOn;
+    });
+
+    if (start != null && end != null) {
+      _selectedEvents.value = _getEventsForRange(start, end);
+    } else if (start != null) {
+      _selectedEvents.value = _getEventsForDay(start);
+    } else if (end != null) {
+      _selectedEvents.value = _getEventsForDay(end);
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     print('${CalendarPage.routename} built');
+
     return Scaffold(
         appBar: AppBar(title: Text(CalendarPage.routename), centerTitle: true),
         bottomNavigationBar: BottomAppBar(
@@ -53,9 +144,296 @@ class CalendarPage extends StatelessWidget {
             ],
           ),
         ),
-        body: Center(
-          child: Text('Hello World'),
-        ));
-  } //build
-
+        body: /*TableCalendar(
+        firstDay: DateTime.utc(2010, 10, 16),
+        lastDay: DateTime.utc(2030, 10, 16),
+        focusedDay: DateTime.now(),
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        calendarFormat: _calendarFormat,
+        /*onFormatChanged: (format) {
+          setState(() {
+            _calendarFormat = format;
+          });
+        },*/
+        selectedDayPredicate: (day) {
+          print('$_selectedDay has been selected');
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          print('$_selectedDay has been selected we hope');
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay; // update `_focusedDay` here as well
+          });
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
+      ),*/
+            Column(children: [
+          ValueListenableBuilder<DateTime>(
+            valueListenable: _focusedDay,
+            builder: (context, value, _) {
+              return _CalendarHeader(
+                focusedDay: value,
+                clearButtonVisible: canClearSelection,
+                onTodayButtonTap: () {
+                  setState(() => _focusedDay.value = DateTime.now());
+                },
+                onClearButtonTap: () {
+                  setState(() {
+                    _rangeStart = null;
+                    _rangeEnd = null;
+                    _selectedDays.clear();
+                    _selectedEvents.value = [];
+                  });
+                },
+                onLeftArrowTap: () {
+                  _pageController.previousPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+                onRightArrowTap: () {
+                  _pageController.nextPage(
+                    duration: Duration(milliseconds: 300),
+                    curve: Curves.easeOut,
+                  );
+                },
+              );
+            },
+          ),
+          TableCalendar<Event>(
+            startingDayOfWeek: StartingDayOfWeek.monday,
+            firstDay: kFirstDay,
+            lastDay: kLastDay,
+            focusedDay: _focusedDay.value,
+            headerVisible: false,
+            selectedDayPredicate: (day) => _selectedDays.contains(day),
+            rangeStartDay: _rangeStart,
+            rangeEndDay: _rangeEnd,
+            calendarFormat: _calendarFormat,
+            rangeSelectionMode: _rangeSelectionMode,
+            eventLoader: _getEventsForDay,
+            /*holidayPredicate: (day) {
+              // Every 20th day of the month will be treated as a holiday
+              return day.day == 20;
+            },*/
+            onDaySelected: _onDaySelected,
+            onRangeSelected: _onRangeSelected,
+            onCalendarCreated: (controller) => _pageController = controller,
+            onPageChanged: (focusedDay) => _focusedDay.value = focusedDay,
+            onFormatChanged: (format) {
+              if (_calendarFormat != format) {
+                setState(() => _calendarFormat = format);
+              }
+            },
+          ),
+          const SizedBox(height: 8.0),
+          Expanded(
+              child: ValueListenableBuilder<List<Event>>(
+                  valueListenable: _selectedEvents,
+                  builder: (context, value, _) {
+                    return ListView.builder(
+                      itemCount: value.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          margin: const EdgeInsets.symmetric(
+                            horizontal: 12.0,
+                            vertical: 4.0,
+                          ),
+                          decoration: BoxDecoration(
+                            border: Border.all(),
+                            borderRadius: BorderRadius.circular(12.0),
+                          ),
+                          child: ListTile(
+                            onTap: () => print('${value[index]}'),
+                            title: Text('${value[index]}'),
+                          ),
+                        );
+                      },
+                    );
+                  }))
+        ]));
+  }
 } //CalendarPage
+
+class _CalendarHeader extends StatelessWidget {
+  final DateTime focusedDay;
+  final VoidCallback onLeftArrowTap;
+  final VoidCallback onRightArrowTap;
+  final VoidCallback onTodayButtonTap;
+  final VoidCallback onClearButtonTap;
+  final bool clearButtonVisible;
+
+  const _CalendarHeader({
+    Key? key,
+    required this.focusedDay,
+    required this.onLeftArrowTap,
+    required this.onRightArrowTap,
+    required this.onTodayButtonTap,
+    required this.onClearButtonTap,
+    required this.clearButtonVisible,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final headerText = DateFormat.yMMM().format(focusedDay);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Row(
+        children: [
+          const SizedBox(width: 16.0),
+          SizedBox(
+            width: 120.0,
+            child: Text(
+              headerText,
+              style: TextStyle(fontSize: 26.0),
+            ),
+          ),
+          IconButton(
+            icon: Icon(Icons.calendar_today, size: 20.0),
+            visualDensity: VisualDensity.compact,
+            onPressed: onTodayButtonTap,
+          ),
+          if (clearButtonVisible)
+            IconButton(
+              icon: Icon(Icons.clear, size: 20.0),
+              visualDensity: VisualDensity.compact,
+              onPressed: onClearButtonTap,
+            ),
+          const Spacer(),
+          IconButton(
+            icon: Icon(Icons.chevron_left),
+            onPressed: onLeftArrowTap,
+          ),
+          IconButton(
+            icon: Icon(Icons.chevron_right),
+            onPressed: onRightArrowTap,
+          ),
+        ],
+      ),
+    );
+  }
+}
+*/
+class CalendarPage extends StatefulWidget {
+  static const route = '/calendar';
+  static const routename = 'Calendar Page';
+
+  @override
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TableCalendar - Basics'),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            IconButton(
+              tooltip: 'Calendar',
+              icon: const Icon(Icons.calendar_month),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Home',
+              icon: const Icon(Icons.home),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, HomePage.route);
+              },
+            ),
+            IconButton(
+              tooltip: 'Check your health status',
+              icon: const Icon(MdiIcons.heartFlash),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, HealthPage.route);
+              },
+            ),
+            IconButton(
+              tooltip: 'Profile',
+              icon: const Icon(Icons.person),
+              onPressed: () {
+                Navigator.pushReplacementNamed(context, ProfilePage.route);
+              },
+            ),
+          ],
+        ),
+      ),
+      body: TableCalendar(
+        firstDay: kFirstDay,
+        lastDay: kLastDay,
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        startingDayOfWeek: StartingDayOfWeek.monday,
+        selectedDayPredicate: (day) {
+          // Use `selectedDayPredicate` to determine which day is currently selected.
+          // If this returns true, then `day` will be marked as selected.
+
+          // Using `isSameDay` is recommended to disregard
+          // the time-part of compared DateTime objects.
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          if (!isSameDay(_selectedDay, selectedDay)) {
+            // Call `setState()` when updating the selected day
+            setState(() {
+              _selectedDay = selectedDay;
+              _focusedDay = focusedDay;
+            });
+            showDialog<String>(
+                context: context,
+                builder: (BuildContext context) => AlertDialog(
+                      title: Text('$selectedDay activity'),
+                      content: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: const [
+                          Text('AlertDialog description'),
+                          SizedBox(height: 20),
+                          Text('This could actually work'),
+                          SizedBox(height: 20),
+                        ],
+                      ),
+                      actions: [
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'Cancel'),
+                          child: const Text('Cancel'),
+                        ),
+                        TextButton(
+                          onPressed: () => Navigator.pop(context, 'OK'),
+                          child: const Text('OK'),
+                        ),
+                      ],
+                    ));
+            //Navigator.pushNamed(context, HomePage.route);
+          }
+        },
+        onFormatChanged: (format) {
+          if (_calendarFormat != format) {
+            // Call `setState()` when updating calendar format
+            setState(() {
+              _calendarFormat = format;
+            });
+          }
+        },
+        onPageChanged: (focusedDay) {
+          // No need to call `setState()` here
+          _focusedDay = focusedDay;
+        },
+      ),
+    );
+  }
+}
