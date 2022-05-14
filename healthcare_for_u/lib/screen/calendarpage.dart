49af17abf6cb3ad7512a1446b75a9b9831338b27
Corinna Semenzato <corinna.semenzato@studenti.unipd.it>
2022-05-14 17:11:50 +0200

    Restyle popup and clean up comments

--- healthcare_for_u/lib/screen/calendarpage.dart
+++ healthcare_for_u/lib/screen/calendarpage.dart
@@ -27,7 +27,7 @@ class _CalendarPageState extends State<CalendarPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('TableCalendar - Basics'),
+        title: Text('${CalendarPage.routename}'),
       ),
       bottomNavigationBar: BottomAppBar(
         color: Colors.white,
@@ -64,7 +64,7 @@ class _CalendarPageState extends State<CalendarPage> {
         ),
       ),
       body: TableCalendar(
-        firstDay: DateTime(2000, 1, 1),
+        firstDay: DateTime(2020, 1, 1),
         lastDay: DateTime(2025, 1, 1),
         focusedDay: _focusedDay,
         calendarFormat: _calendarFormat,
@@ -84,40 +84,38 @@ class _CalendarPageState extends State<CalendarPage> {
               _selectedDay = selectedDay;
               _focusedDay = focusedDay;
             });
-            showDialog<String>(
-                context: context,
-                builder: (BuildContext context) => AlertDialog(
-                      title: Text('$selectedDay activity'),
-                      content: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          FutureBuilder(
-                              future: _fetchSteps(selectedDay),
-                              builder: (context, snapshot) {
-                                if (snapshot.hasData) {
-                                  final steps = snapshot.data as double;
-                                  return Text('Walked $steps');
-                                } else {
-                                  return CircularProgressIndicator();
-                                }
-                              }),
-                          SizedBox(height: 20),
-                        ],
-                      ),
-                      actions: [
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'Cancel'),
-                          child: const Text('Cancel'),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.pop(context, 'OK'),
-                          child: const Text('OK'),
-                        ),
-                      ],
-                    ));
-            //Navigator.pushNamed(context, HomePage.route);
           }
+          showDialog<String>(
+              context: context,
+              builder: (BuildContext context) => AlertDialog(
+                    title: Text('${DateFormat.yMMMd().format(selectedDay)}'
+                        ' activities'),
+                    alignment: Alignment.center,
+                    content: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.min,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        FutureBuilder(
+                            future: _fetchSteps(selectedDay),
+                            builder: (context, snapshot) {
+                              if (snapshot.hasData) {
+                                final steps = snapshot.data as double;
+                                return _textSteps(steps, 10000);
+                              } else {
+                                return CircularProgressIndicator();
+                              }
+                            }),
+                        SizedBox(height: 20),
+                      ],
+                    ),
+                    actions: [
+                      TextButton(
+                        onPressed: () => Navigator.pop(context, 'Cancel'),
+                        child: const Text('Cancel'),
+                      ),
+                    ],
+                  ));
         },
         onFormatChanged: (format) {
           if (_calendarFormat != format) {
@@ -133,7 +131,7 @@ class _CalendarPageState extends State<CalendarPage> {
         },
       ),
     );
-  }
+  } // build
 
   Future<double?> _fetchSteps(DateTime day) async {
     FitbitActivityTimeseriesDataManager fitbitActivityTimeseriesDataManager =
@@ -150,5 +148,39 @@ class _CalendarPageState extends State<CalendarPage> {
       resource: fitbitActivityTimeseriesDataManager.type,
     )) as List<FitbitActivityTimeseriesData>;
     return steps[0].value as double;
+  } // _fetchSteps
+
+  Widget _textSteps(double steps, double goal) {
+    return RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: "You walked ",
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+          ),
+          children: [
+            TextSpan(
+              text: "${steps.toInt()}",
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const TextSpan(
+              text: " steps out of your goal of ",
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            TextSpan(
+              text: "${goal.toInt()}!",
+              style: const TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ],
+        ));
   }
-}
+} //_CalendarPageState
